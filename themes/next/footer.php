d8c274b7b995fe1e2b62f32dc8f2c6705ca56d42
Jaideep <78426970+jaideep-git@users.noreply.github.com>
2022-03-05 22:54:42 -0500

    Customizing footer

--- themes/next/footer.php
+++ themes/next/footer.php
@@ -14,48 +14,21 @@
 	<footer id="colophon" class="site-footer">
 		<div class="grid-container grid-x">
 			<div class="cell large-3 coloumn1">
-				<img src="<?php echo get_template_directory_uri() . '/assets/img/logo.png'?>" alt="..">
-				<div class="footer-info"> <i class="fa fa-phone footer-icon"></i><p>1800-000-000</p></div>
-				<div class="footer-info"> <i class="fa fa-envelope footer-icon"></i><p>info@instantgroceries.com</p></div>
-				<div class="social-icons">
-          <i class="fab fa-facebook icon"></i>
-          <i class="fab fa-twitter icon"></i>
-          <i class="fab fa-instagram icon"></i>
-          <i class="fab fa-pinterest-p icon"></i>
-        </div>
+			<?php the_custom_logo(); ?>
+				<?php dynamic_sidebar( 'footer Widget 1' );?>
+				<?php dynamic_sidebar( 'social-media-links' );?>
 			</div>
 
 			<div class="cell large-3 coloumn">
-				<h4 class="footer-head">About Us</h4>
-				<ul>
-					<li>Privacy Policy</li>
-					<li>Refund & Return Policy</li>
-					<li>Contact Us</li>
-					<li>Blog</li>
-					<li>Careers</li>
-				</ul>
+				<?php dynamic_sidebar( 'footer Widget 2' );?>
 			</div>
 
 			<div class="cell large-3 coloumn">
-				<h4 class="footer-head">My Account</h4>
-					<ul>
-						<li>My Orders</li>
-						<li>Wishlist</li>
-						<li>Return or Refund</li>
-						<li>Shopping Cart</li>
-						<li>Payments</li>
-					</ul>
+				<?php dynamic_sidebar( 'footer Widget 3' );?>
 			</div>
 
 			<div class="cell large-3 coloumn">
-				<h4 class="footer-head">Our Store Locations</h4>
-					<ul>
-						<li>Torronto</li>
-						<li>Oshawa</li>
-						<li>Brampton</li>
-						<li>Kitchner</li>
-						<li>Miississauga</li>
-					</ul>
+				<?php dynamic_sidebar( 'footer Widget 4' );?>
 			</div>
 		</div>
 		<p class="text-center footer-copyright"> &copy; Created by a <a href="http://jaideep.ca" target="_blank">Jaideep.</a></p>
