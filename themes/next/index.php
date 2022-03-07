693e5e74080547ca2a20a3c3cf793dfde166e0a1
Jaideep <78426970+jaideep-git@users.noreply.github.com>
2022-03-06 21:18:58 -0500

    Blog page styling

--- themes/next/index.php
+++ themes/next/index.php
@@ -15,43 +15,50 @@
 get_header();
 ?>
 
-	<main id="primary" class="site-main grid-container">
+	<main id="primary" class="site-main grid-container ">
+		<h2 class="page-heading">Blog</h2>
+		<article class="grid-x">
+			<?php
+			if ( have_posts() ) :
 
-		<?php
-		if ( have_posts() ) :
-
-			if ( is_home() && ! is_front_page() ) :
-				?>
-				<header>
-					<h1 class="page-title screen-reader-text"><?php single_post_title(); ?></h1>
-				</header>
+				if ( is_home() && ! is_front_page() ) :
+					?>
+					<header>
+						<h1 class="page-title screen-reader-text"><?php single_post_title(); ?></h1>
+					</header>
+					<?php
+				endif; ?>
+				<div class="cell large-12 ">
 				<?php
-			endif;
+				/* Start the Loop */
+				while ( have_posts() ) : ?>
+					
+							<?php 
+								the_post();
 
-			/* Start the Loop */
-			while ( have_posts() ) :
-				the_post();
+										/*
+										* Include the Post-Type-specific template for the content.
+										* If you want to override this in a child theme, then include a file
+										* called content-___.php (where ___ is the Post Type name) and that will be used instead.
+										*/
+										get_template_part( 'template-parts/content', 'archive' );
 
-				/*
-				 * Include the Post-Type-specific template for the content.
-				 * If you want to override this in a child theme, then include a file
-				 * called content-___.php (where ___ is the Post Type name) and that will be used instead.
-				 */
-				get_template_part( 'template-parts/content', get_post_type() );
+									endwhile;
 
-			endwhile;
+										the_posts_navigation();
 
-			the_posts_navigation();
+									else :
 
-		else :
+										get_template_part( 'template-parts/content', 'none' );
 
-			get_template_part( 'template-parts/content', 'none' );
-
-		endif;
-		?>
-
+									endif;
+									?>
+						</div>
+				
+		</article>
+	
 	</main><!-- #main -->
 
 <?php
-get_sidebar();
+
 get_footer();
---
+++ themes/next/template-parts/content-archive.php
@@ -0,0 +1,46 @@
+<?php
+/**
+ * Template part for displaying posts
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package NextTheme
+ */
+
+?>
+
+<article id="post-<?php the_ID(); ?>" class="blog-single-post">
+	<section class="grid-x">
+		<div class="blog-post-img cell large-3">
+			<?php next_post_thumbnail(); ?>
+		</div>
+		<div class="cell large-9">
+			<?php
+			if ( is_singular() ) :
+				the_title( '<h4 class="entry-title">', '</h4>' );
+			else :
+				the_title( '<h4 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h4>' );
+			endif;
+
+			if ( 'post' === get_post_type() ) :
+				?>
+				<div class="entry-meta blog-post-date">
+					<?php
+					next_posted_on();
+					next_posted_by();
+					?>
+				</div><!-- .entry-meta -->
+			<?php endif; ?>
+			<div class="blog-post-content">
+				<?php the_excerpt(); ?>
+				<button id="submit"><a class="movie-page" href="<?php the_permalink() ?>">Read More</a></button>
+			</div>
+		</div>
+		<header class="entry-header ">
+		
+		</header><!-- .entry-header -->
+		
+		
+	</section>
+
+</article>
