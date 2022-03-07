a4f1ab4eca8965f963ca8fa08a9ad5f4744d7225
Jaideep <78426970+jaideep-git@users.noreply.github.com>
2022-03-06 21:20:57 -0500

    Woocommerce layout edit

--- themes/next/inc/woocommerce.php
+++ themes/next/inc/woocommerce.php
@@ -12,3 +12,5 @@ function next_use_block_editor_for_post_type($use_block_editor, $post_type){
   return $use_block_editor;
 }
 add_filter( 'use_block_editor_for_post_type', 'next_use_block_editor_for_post_type', 10, 2 );
+
+add_action( 'woocommerce_single_product_summary', 'woocommerce_template_single_title', 4 );
