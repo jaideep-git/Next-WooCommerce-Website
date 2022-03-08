d4ac2290cb8d1a77c8cd7fa3d1643e7883aff053
Jaideep <78426970+jaideep-git@users.noreply.github.com>
2022-03-08 01:29:52 -0500

    woocommerce title

--- themes/next/template-parts/content.php
+++ themes/next/template-parts/content.php
@@ -15,7 +15,7 @@
 			<?php
 			if ( !is_singular('product') ) {
 				if ( is_singular() ) :
-					the_title( '<h1 class="entry-title">', '</h1>' );
+					the_title( '<h2 class="entry-title">', '</h2>' );
 				else :
 					the_title( '<h2 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h2>' );
 				endif;
@@ -23,7 +23,7 @@
 			
 			if ( 'post' === get_post_type() ) :
 				?>
-				<div class="entry-meta">
+				<div class="entry-meta post-date">
 					<?php
 					next_posted_on();
 					next_posted_by();
