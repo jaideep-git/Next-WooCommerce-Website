d25204f6d7994628421c4809a25ce1cbd15cd2bf
Jaideep <78426970+jaideep-git@users.noreply.github.com>
2022-03-27 00:30:11 -0400

    Update footer.php

--- themes/next/footer.php
+++ themes/next/footer.php
@@ -8,9 +8,7 @@
  *
  * @package NextTheme
  */
-
 ?>
-
 	<footer id="colophon" class="site-footer">  
 		<div class="grid-container grid-x">
 			<h3 class="recent-posts">Recent Posts</h3>
@@ -42,26 +40,25 @@
 				?>  
 			</div>
 			<div class="cell large-3 coloumn1">
-			<?php the_custom_logo(); ?>
-				<?php dynamic_sidebar( 'footer Widget 1' );?>
-				<?php dynamic_sidebar( 'social-media-links' );?>
-			</div>
+                <?php the_custom_logo(); ?>
+                    <?php dynamic_sidebar( 'footer Widget 1' );?>
+                    <?php dynamic_sidebar( 'social-media-links' );?>
+                </div>
 
-			<div class="cell large-3 coloumn">
-				<?php dynamic_sidebar( 'footer Widget 2' );?>
-			</div>
+                <div class="cell large-3 coloumn">
+                    <?php dynamic_sidebar( 'footer Widget 2' );?>
+                </div>
 
-			<div class="cell large-3 coloumn">
-				<?php dynamic_sidebar( 'footer Widget 3' );?>
-			</div>
+                <div class="cell large-3 coloumn">
+                    <?php dynamic_sidebar( 'footer Widget 3' );?>
+                </div>
 
-			<div class="cell large-3 coloumn">
-				<?php dynamic_sidebar( 'footer Widget 4' );?>
-			</div>
-		</div>
+                <div class="cell large-3 coloumn">
+                    <?php dynamic_sidebar( 'footer Widget 4' );?>
+                </div>
+            </div>
 		<p class="text-center footer-copyright"> &copy; Created by a <a href="http://jaideep.ca" target="_blank">Jaideep.</a></p>
 	</footer>
-
 <?php wp_footer();?>
 
 </body>
