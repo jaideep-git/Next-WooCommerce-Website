28ff7920a65a9e4fbdfba9f5e560d0df85990a4c
Jaideep <78426970+jaideep-git@users.noreply.github.com>
2022-03-26 23:16:52 -0400

    Update content-none.php

--- themes/next/template-parts/content-none.php
+++ themes/next/template-parts/content-none.php
@@ -9,7 +9,7 @@
 
 ?>
 
-<section class="no-results not-found">
+<section class="no-results not-found grid-container">
 	<header class="page-header">
 		<h1 class="page-title"><?php esc_html_e( 'Nothing Found', 'next' ); ?></h1>
 	</header><!-- .page-header -->
