944d7f83b06a598fcb7fc44756378bc1a50039c8
Jaideep <78426970+jaideep-git@users.noreply.github.com>
2022-03-26 23:14:21 -0400

    create custom archive template

---
+++ themes/next/archive-next_offer.php
@@ -0,0 +1,46 @@
+<?php
+/**
+ * The template for displaying archive pages
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package NextTheme
+ */
+
+get_header();
+?>
+
+	<main id="primary" class="site-main grid-container">
+
+		<?php if ( have_posts() ) : ?>
+
+			<header class="page-header">
+				<h2 class="page-title">Latest Offers</h2>
+				<?php
+					the_archive_description( '<div class="archive-description">', '</div>' );
+				?>
+			</header><!-- .page-header -->
+			
+            <div class="grid-x offer-page"> 
+                <?php
+                /* Start the Loop */
+                    while ( have_posts() ) :
+                        the_post();
+                        /*
+                        * Include the Post-Type-specific template for the content.
+                        * If you want to override this in a child theme, then include a file
+                        * called content-___.php (where ___ is the Post Type name) and that will be used instead.
+                        */
+                        get_template_part( 'template-parts/content', 'custom' );
+                    endwhile;
+                        the_posts_navigation();
+                    else :
+                        get_template_part( 'template-parts/content', 'none' );
+                    endif;
+                ?>
+            </div>
+	</main><!-- #main -->
+
+<?php
+get_sidebar();
+get_footer();
