9ce04ff2cb7c99d7f069d0a0b15631aa8b26c7d1
Jaideep <78426970+jaideep-git@users.noreply.github.com>
2022-03-28 23:53:48 -0400

    header commenting

--- themes/next/header.php
+++ themes/next/header.php
@@ -18,7 +18,6 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="profile" href="https://gmpg.org/xfn/11">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css"/>
-
 	<?php wp_head(); ?>
 </head>
 
@@ -27,9 +26,12 @@
 <div id="page" class="site">
 	<a class="skip-link screen-reader-text" href="#primary"><?php esc_html_e( 'Skip to content', 'next' ); ?></a>
 
+    <!------ Secondary Navbar ------> 
 	<div class="top-bar">
         <div class="grid-container top-bar">
+            <!-- social media widget -->
             <?php dynamic_sidebar( 'social-media-links' );?>
+            <!-- secondary nav menu -->
             <?php
                 if ( has_nav_menu('menu-secondary') ){
                     wp_nav_menu(
@@ -43,21 +45,25 @@
         </div>
 	</div>
 
+    <!------ Main Navbar ------>
 	<header id="masthead" class="site-header">
-        <div class="grid-container top-bar " id="navbar">
+        <div class="grid-container main-nav" id="navbar">
+            <!-- Site Logo -->
             <div class="logo">
                 <?php the_custom_logo(); ?>
             </div>
             <div class="search-bar">
                 <ul class="menu-bar">
-                    <?php aws_get_search_form( true ); ?>
+                    <?php aws_get_search_form( true ); // search Bar 
+                    ?> 
                 </ul>
+                <!-- Cart -->
                 <a href="<?php echo wc_get_cart_url(); ?>"><i class="fas fa-shopping-cart cart-icon"></i></a>
-                
             </div>
         </div>
         <nav id="site-navigation" class="main-navigation">
             <div class="grid-container">
+                <!-- Primary nav menu -->
                 <?php
                 if ( has_nav_menu('menu-primary') ){
                     wp_nav_menu(
